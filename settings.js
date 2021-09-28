2e98883445da8ac1edb2127e75ff333fb96bc262
kyle <mr.th2.90@gmail.com>
2021-09-28 09:11:27 +0700

    port need to be number to deploy heroku

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: +process.env.PORT || 3000,
     env: process.env.NODE_ENV || 'development',
     host: process.env.HOST || 'localhost'
 }
